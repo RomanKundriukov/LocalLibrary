ae9be3e9555fa07641efc8926247550cbdef879f
Roman Kundriukov <r.kundriukov@web.de>
2024-04-21 12:31:19 +0200

    neue Projekt

---
+++ LocalLibrary/obj/Debug/net9.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("LocalLibrary.MainApplication, LocalLibrary, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc643fed6ac17631b12c.MainApplication.class, crc643fed6ac17631b12c.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
