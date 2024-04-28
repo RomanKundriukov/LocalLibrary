4dbd3dd0316e1ea6a7587f35b80bf900861f7366
Roman Kundriukov <r.kundriukov@web.de>
2024-04-28 10:46:55 +0200

    muss noch Service für die Datenbank beenden

---
+++ LocalLibrary/Services/SqliteCommand.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace LocalLibrary.Services
+{
+    public static class SqliteCommand
+    {
+        private static SqlConnection Connection(string path)
+        {
+            try
+            {
+                var conn = new SqlConnection(path);
+                conn.Open();
+                return conn;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void SetParhDb(string path, string nameDb, string command)
+        {
+            using var cmd = new SqlCommand()
+            {
+                Connection = Connection(path),
+                CommandType = CommandType.Text,
+                CommandText = command
+            };
+
+            cmd.Connection.Close();
+        }
+    }
+}
